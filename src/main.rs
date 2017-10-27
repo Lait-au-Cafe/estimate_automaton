d87e72e183a6e20488fccae9edb89df771258aa7
Lait-au-Cafe <laitaucafe8@gmail.com>
2017-10-28 00:05:29 +0900

    完成してないがとりあえず完了

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ struct Node {
 }
 impl Node {
     fn new(s: char) -> Node {
-        let mut hm = HashMap::new();
+        let hm = HashMap::new();
         return Node{state: s, path: hm};
     }
     fn get_path(&self, c: char) -> Option<usize> {
@@ -45,6 +45,40 @@ impl Node {
 		}
 		return;
 	}
+
+    fn create_path(
+        mut ptr: i32,               // 探索開始場所
+        n_ptr: &mut usize,      // 出発ノード
+        n_list: &mut Vec<Node>, // ノードのリスト
+        l_ptr: usize,           // 現在のリストの位置
+        i_list: &mut Vec<Input> // データリスト
+    ) {
+        // 既存のノードの中から飛び先を探す
+        // let mut ptr = *n_ptr as i32;
+        Node::scan(i_list[l_ptr].output, &mut ptr, &n_list);
+
+        if ptr < 0 {
+            // 新しくノードを作成
+            n_list.push(Node::new(i_list[l_ptr].output));
+            n_list[*n_ptr].set_path(i_list[l_ptr].input, *n_ptr + 1);
+            *n_ptr += 1;
+        } else {
+            // リンクを張って移動
+            i_list[l_ptr].push_link(Link::new(*n_ptr, ptr as usize));
+            n_list[*n_ptr].set_path(i_list[l_ptr].input, ptr as usize);
+            *n_ptr = ptr as usize;
+        }
+    }
+}
+impl ToString for Node {
+    fn to_string(&self) -> String {
+        return format!("{},{},{}", 
+            self.state, 
+            self.get_path('g').map(|i| i.to_string())
+                .unwrap_or("?".to_owned()), 
+            self.get_path('w').map(|i| i.to_string())
+                .unwrap_or("?".to_owned()));
+    }
 }
 
 struct Input {
@@ -58,7 +92,7 @@ impl Input {
     }
 
 	fn scan_link(ptr: &mut i32, list: &mut Vec<Input>) -> Option<Link> {
-		let mut res: Option<Link> = None;
+		let res: Option<Link>;
 		loop {
 			if *ptr < 0 {
 				panic!("Failed to Search link. ");
@@ -91,16 +125,16 @@ impl Input {
             .map_err(|e|"Cannot read file : ".to_owned() + e.description())?;
     
         // convert
-        let mut raw = String::from_utf8(buf)
+        let raw = String::from_utf8(buf)
             .map_err(|e|e.description().to_owned())?;
 
         let mut list: Vec<Input> = Vec::new();
         for s in raw.split('\n') {
-            let mut cs = s.chars().collect::<Vec<char>>();
+            let cs = s.chars().collect::<Vec<char>>();
             Regex::new("^[gw],[gw]$")
                 .map_err(|e| e.description().to_owned())?
                 .find(s)
-                .and_then(|mat| {
+                .and_then(|_| {
                     list.push(Input{
                         input: *cs.last().unwrap(),
                         output: *cs.first().unwrap(),
@@ -115,11 +149,12 @@ impl Input {
 }
 
 struct Link {
-    dest: usize
+    from: usize,
+    to: usize
 }
 impl Link {
-    fn new(i: usize) -> Link {
-        return Link{dest: i};
+    fn new(f: usize, t: usize) -> Link {
+        return Link{from: f, to: t};
     }
 }
 
@@ -141,41 +176,46 @@ fn main() {
         // すでにパスが存在するかのチェック
         match node_list[node_ptr].get_path(input_list[list_ptr].input) {
             Some(index) => {
-                let dest = &node_list[index];
-                if dest.state == input_list[list_ptr].output {
+                if node_list[index].state 
+                        == input_list[list_ptr].output {
                     // すでに飛び先が存在し, データと矛盾しない場合
                     node_ptr = index;
                 } else {
                     // 飛び先が存在するがデータと矛盾する場合
 					// 入力リストをさかのぼってリンクを見つけて解除
-					let mut lptr = list_ptr;
-					let link: Link = 
-						Input::scan_link(&mut (lptr as i32), &mut input_list).unwrap();
-					
+					let mut lptr = list_ptr as i32;
+					let link: Link = Input::scan_link(
+                                        &mut lptr, 
+                                        &mut input_list).unwrap();
+				    
+                    list_ptr = lptr as usize;
+                    node_ptr = link.from;
+
 					// 新しい飛び先を探す
-					let mut ptr = link.dest as i32;
-					Node::scan(input_list[list_ptr].output, &mut ptr, &node_list);
-					
+                    Node::create_path(
+                        (link.to as i32) - 1,
+                        &mut node_ptr,
+                        &mut node_list,
+                        list_ptr, 
+                        &mut input_list);
+                        
                 }
             }, 
-            Node => {
+            None => {
                 // 飛び先が存在しない場合
-                let mut ptr = node_ptr as i32;
-                Node::scan(input_list[list_ptr].output, &mut ptr, &node_list);
-				
-                if ptr < 0 {
-                    // 新しくノードを作成
-                    node_list.push(Node::new(input_list[list_ptr].output));
-                    node_list[node_ptr].set_path(input_list[list_ptr].input, node_ptr+1);
-                    node_ptr += 1;
-                } else {
-                    // リンクを張って移動
-                    input_list[list_ptr].push_link(Link::new(ptr as usize));
-					node_list[node_ptr].set_path(input_list[list_ptr].input, ptr as usize);
-					node_ptr = ptr as usize;
-                }
+                let ptr = node_ptr;
+                Node::create_path(
+                    ptr as i32,
+                    &mut node_ptr,
+                    &mut node_list,
+                    list_ptr, 
+                    &mut input_list);
             },
         }
         list_ptr += 1;
+    }
+
+    for (i, node) in node_list.iter().enumerate() {
+        println!("{}:{}", i, node.to_string());
     }
 }
